9683acb4285ed3cbc3feb3766b6089e7b7cc001c
henryxie <hman821223@gmail.com>
2024-02-16 00:26:09 +0800

    Rename folder from WhereToBuy to wheretobuy

---
+++ pages/[[locale]]/wheretobuy/index.vue
@@ -0,0 +1,12 @@
+<template></template>
+
+<script setup lang="ts">
+definePageMeta({
+  middleware: () => {
+    throw createError({
+      statusCode: 404,
+      statusMessage: "Page Not Found",
+    });
+  },
+});
+</script>
